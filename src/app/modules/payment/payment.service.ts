ff806fbc46cff79989cabdfdf66addd363892a69
AbdulAbdulSatterism <abdulsatter.ism@gmail.com>
2025-03-19 22:13:44 +0600

    update payment success url

--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -43,10 +43,9 @@ const createCheckoutSessionService = async (
       payment_method_types: ['card'],
       line_items: lineItems,
       mode: 'payment',
-      success_url:
-        'https://yourapp.com/success?session_id={CHECKOUT_SESSION_ID}',
-      // success_url: 'http://192.168.10.33:3000',
-      cancel_url: 'https://yourapp.com/cancel',
+      success_url: 'https://oegfitness.com/paymentSuccess',
+
+      cancel_url: 'https://oegfitness.com',
       metadata: {
         userId,
         appointmentId,
@@ -114,53 +113,6 @@ const handleStripeWebhookService = async (event: Stripe.Event) => {
       console.log(`Unhandled event type ${event.type}`);
   }
 };
-
-// const getAllPayment = async () => {
-//   const result = await Payment.aggregate([
-//     {
-//       $match: { status: 'COMPLETED' }, // Filter only completed payments
-//     },
-//     {
-//       $lookup: {
-//         from: 'users', // The name of the User collection in MongoDB
-//         localField: 'userId',
-//         foreignField: '_id',
-//         as: 'user',
-//       },
-//     },
-//     {
-//       $unwind: {
-//         path: '$user',
-//         preserveNullAndEmptyArrays: true, // In case a user is missing
-//       },
-//     },
-//     {
-//       $group: {
-//         _id: null, // Group all documents to compute the subtotal
-//         totalAppointmentPrice: { $sum: '$appointmentPrice' }, // Sum up all appointment prices
-//         payments: { $push: '$$ROOT' }, // Push all documents into an array
-//       },
-//     },
-//     {
-//       $project: {
-//         _id: 0, // Hide _id field
-//         subtotal: '$totalAppointmentPrice', // Rename total price as subtotal
-//         payments: {
-//           _id: 1,
-//           // userId: 1,
-//           transactionId: 1,
-//           appointmentPrice: 1,
-//           status: 1,
-//           createdAt: 1,
-//           updatedAt: 1,
-//           user: { _id: 1, name: 1, email: 1 }, // Only include necessary fields from User
-//         },
-//       },
-//     },
-//   ]);
-
-//   return result.length > 0 ? result[0] : { subtotal: 0, payments: [] };
-// };
 
 const getAllPayment = async (query: Record<string, unknown>) => {
   const { page, limit } = query;
