871a1ff835742782e61bd3fb21e1af0b1f645fc8
AbdulSatterism <abdulsatter.ism@gmail.com>
2025-06-30 18:25:13 +0600

    add new cors origin for dashboard

--- src/app.ts
+++ src/app.ts
@@ -20,6 +20,7 @@ app.use(
     origin: [
       '*',
       'https://oegfitness.com',
+      'https://dashboard.oegfitness.com',
       'http://localhost:3000',
       'http://localhost:3001',
     ],
